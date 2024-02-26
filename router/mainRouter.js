20217a57fb43d04a0765a8a60b4c4434228fc68b
Mika Otter <mi.otter.side@gmail.com>
2024-02-26 22:32:54 +0100

    DONE: Main Page CSS

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -2,7 +2,7 @@ const useRouter = require("express").Router();
 
 useRouter.get("/", async (req, res) => {
     try {
-        res.render("base.twig", {
+        res.render("layouts/home.twig", {
             title: "Empleez",
         });
     } catch (error) {
